275b87b546fb283812d8631c2e0c0fe7e37bb7c8
EC2 Default User <ec2-user@ip-172-31-83-82.ec2.internal>
2024-12-07 20:35:51 +0000

    bash script files

---
+++ message.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+name=Nazgul
+echo "Hello my name is $name"
+echo "$name is currently studying Linux"
+echo "Nazgul  loves DevOps"
+echo "26*30*12"
---
+++ task.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+my_name=Nazgul
+fav_food=Plov
+fav_season=Summer
+
+echo "Hello my name is $my_name, and my favorite food is $fav_food, if your favorite season is $fav_season, let's get along"
